5d80bc0bc8d85de4b7ab4b773127d480ed8c197b
mackarelfish <mackarelfish@mackarelfishs-MacBook-Pro.local>
2020-02-11 11:40:46 +0700

    fetching with fetchmemez

--- src/components/meme-app/index.js
+++ src/components/meme-app/index.js
@@ -4,40 +4,12 @@ import "./MemeApp.css";
 import Meme from "../meme";
 
 function MemeApp() {
-    const [memes, setMemes] = useState({ loading: true, memes: [] });
-    const [currPage, setCurrPage] = useState(1);
-    const [memesPerPage, setMemesPerPage] = useState(10);
-
-    function fetchMemez() {
-        setMemes(({ memes }) => ({ loading: true, memes }));
-        setCurrPage(1);
-        fetch("https://meme-api.herokuapp.com/gimme/100")
-            .then(r => r.json())
-            .then(d => {
-                d.loading = false;
-                setMemes({ loading: d.loading, memes: d.memes });
-                localStorage.setItem("memez_data", JSON.stringify(d));
-            });
-    }
-
-    function prevMemez() {
-        setCurrPage(prev => (prev <= 1 ? 1 : prev - 1));
-    }
-
-    function nextMemez() {
-        let maxPage = Math.ceil(memes.memes.length / memesPerPage);
-        setCurrPage(prev => (prev === maxPage ? maxPage : prev + 1));
-    }
-
-    function handlePerPage(e) {
-        setCurrPage(1);
-        setMemesPerPage(e.target.value);
-    }
-
+    const [memes, setMemes] = useState({ loading: true, memes: [], count: 0 });
     useEffect(() => {
         if (localStorage.getItem("memez_data")) {
             setMemes({
                 loading: false,
+                count: JSON.parse(localStorage.getItem("memez_data")).count,
                 memes: JSON.parse(localStorage.getItem("memez_data")).memes
             });
             return;
@@ -45,39 +17,30 @@ function MemeApp() {
         fetchMemez();
     }, []);
 
+    function fetchMemez() {
+        setMemes(({ memes, count }) => ({ loading: true, memes, count }));
+        fetch("https://meme-api.herokuapp.com/gimme/10")
+            .then(r => r.json())
+            .then(d => {
+                d.loading = false;
+                setMemes(prev => {
+                    return {
+                        loading: d.loading,
+                        count: [...prev.memes, ...d.memes].length,
+                        memes: [...prev.memes, ...d.memes]
+                    };
+                });
+                localStorage.setItem("memez_data", JSON.stringify(d));
+            });
+    }
+
     return (
         <div className="App">
-            {memes.loading ? (
-                <h1>Loading...</h1>
-            ) : (
-                memes.memes
-                    .slice(
-                        (currPage - 1) * memesPerPage,
-                        currPage * memesPerPage
-                    )
-                    .map(items => {
-                        return <Meme {...items} />;
-                    })
-            )}
-            {/* <button onClick={fetchMemez}>fetch the memez</button>
-            <button onClick={() => setCurrPage(1)}>first page</button>
-            <button onClick={prevMemez}>prev page</button>
-            <button onClick={nextMemez}>next page</button>
-            <button
-                onClick={() =>
-                    setCurrPage(Math.ceil(memes.memes.length / memesPerPage))
-                }
-            >
-                last page
-            </button>
-
-            <h1>{currPage}</h1>
-            <input
-                type="number"
-                name="nums"
-                onChange={handlePerPage}
-                value={memesPerPage}
-            /> */}
+            {memes.memes.map(items => {
+                return <Meme {...items} />;
+            })}
+            {memes.loading && <h1>Loading..</h1>}
+            <button onClick={fetchMemez}>fetch the memez</button>
         </div>
     );
 }
--- src/components/meme/index.js
+++ src/components/meme/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function Meme(props) {
-    return <h1>{props.url}</h1>;
+function Meme({ subreddit, title, url }) {
+    return (
+        <div>
+            <img src={url} alt="meme_img" style={{ height: "300px" }} />
+            <h1>{title}</h1>
+            <p>/r/{subreddit}</p>
+        </div>
+    );
 }
 
 export default Meme;
