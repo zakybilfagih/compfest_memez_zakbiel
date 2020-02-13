abc913a4caa60d27a78361de692d38922310cd9d
jak biel <zmaster365@gmail.com>
2020-02-13 23:16:42 +0700

    Updates

---
+++ precache-manifest.8963acaefb27d8b67e9489c495b7c6a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d56960bb32ed98e94e072aeb0b5d4a4",
+    "url": "/compfest_memez_zakbiel/index.html"
+  },
+  {
+    "revision": "ce2539b651204708ee38",
+    "url": "/compfest_memez_zakbiel/static/css/main.72f38ca5.chunk.css"
+  },
+  {
+    "revision": "b38fc5e7e584605a8457",
+    "url": "/compfest_memez_zakbiel/static/js/2.9ff543dc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/compfest_memez_zakbiel/static/js/2.9ff543dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce2539b651204708ee38",
+    "url": "/compfest_memez_zakbiel/static/js/main.dc48d7b5.chunk.js"
+  },
+  {
+    "revision": "1e27a1e8f8bba6c03cb9",
+    "url": "/compfest_memez_zakbiel/static/js/runtime-main.7ef100d2.js"
+  }
+]);
